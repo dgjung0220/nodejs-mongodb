74e98dd62d375b07f8c2381913d29dba0b4cc8d1
Donggoo Jung <dgjung0220@gmail.com>
2017-08-01 11:36:15 +0900

    template by handlebars

--- imgPloadr.io/index.js
+++ imgPloadr.io/index.js
@@ -1,26 +1,10 @@
 var express = require('express');
+var config = require('./server/configure');
 var app = express();
 
-var route = require('./route.js');
-
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var morgan = require('morgan');
-var methodOverride = require('method-override');
-var errorHandler = require('errorhandler');
-
 app.set('port', process.env.PORT || 3000);
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: ture}));
-app.use(dobyParser.json());
-app.use(methodOverride());
-app.use(cookieParser('some-scret-value-here'));
-
-app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');  
-
-app.use('/', route);
+app = config(app);
 
 app.listen(app.get('port'), function() {
   console.log('Server up : http://localhost: ' + app.get('port'));
---
+++ imgPloadr.io/server/configure.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var route = require('./route');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+var moment = require('moment');
+var multer = require('multer');
+
+module.exports = function(app) {
+    app.engine('handlebars',exphbs.create({
+        defaultLayout: 'main',
+        layoutsDir: app.get('views') + '/layouts',
+        partialsDir: app.get('views') + '/partials',
+        helpers: {
+            timeago: function(timestamp) {
+                console.log(timestamp);
+                return moment(timestamp).startOf('minute').fromNow();
+            }
+        }
+    }).engine);
+    app.set('view engine', 'handlebars');
+
+    app.use(morgan('dev'));
+    var upload = multer({ dest: path.join(__dirname, 'public/upload/temp')});
+
+    app.use(methodOverride());
+    app.use(cookieParser('some-secret-value-here'));
+
+    app.use('/', route);
+    
+    app.use('/public/', express.static(path.join(__dirname, '../public')));
+    if ('development' === app.get('env')) {
+       app.use(errorHandler());
+    }
+
+    return app;
+}
